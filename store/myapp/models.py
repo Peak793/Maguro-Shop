c37592f8d139849a46f36938a99d8c84c6b57302
Peak793 <69148111+Peak793@users.noreply.github.com>
2021-11-18 03:10:09 +0700

    update 18/11/2021 #2
    -add product models
    -image.url still doesn't work

--- store/myapp/models.py
+++ store/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    code = models.CharField(max_length=10,unique=True)
+    name = models.CharField(max_length=100,null=True,blank=True)
+    price = models.FloatField(default=0)
+    description = models.TextField(null=True, blank=True)
+    available = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    image = models.FileField(upload_to='upload',null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- store/store/settings.py
+++ store/store/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -121,12 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'/static'),
 ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
