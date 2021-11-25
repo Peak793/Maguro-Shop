25a17238442cde619392770042fe711ec11263be
Peak793 <69148111+Peak793@users.noreply.github.com>
2021-11-25 15:39:42 +0700

    -update 25/11/2021

--- store/myapp/urls.py
+++ store/myapp/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from myapp import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('store/',views.store,name='store'),
-    path('store/<slug:slug>/',views.detail,name='detail')
+    path('store/<slug:slug>/',views.detail,name='detail'),
+    re_path(r'cart/add/(?P<slug>[\w-]+)/$', views.cart_add, name ='cart_add'),
+    re_path(r'cart/list/$', views.cart_list, name ='cart_list'),
 ]
--- store/myapp/views.py
+++ store/myapp/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render,get_object_or_404
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
-from .models import Category, Product,Reccom
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Category, Product, Reccom
 # Create your views here.
+
+
 def index(request):
     reccom = Reccom.objects.all()
-    return render(request, 'index.html',{'reccom':reccom,})
+    for x in reccom:
+        print(x.product.price)
+    return render(request, 'index.html', {'reccom': reccom, })
+
 
 def store(request):
-    products = Product.objects.filter(available = True)
+    products = Product.objects.filter(available=True)
+    c = request.GET.get('categoryid')
+
+    if c:
+        products = products.filter(category_id=int(c))
+
     categories = Category.objects.all()
-                                                                
-    paginator = Paginator(products,12)
+
+    paginator = Paginator(products, 12)
     page = request.GET.get('page')
     try:
         products = paginator.page(page)
@@ -19,10 +30,51 @@ def store(request):
     except EmptyPage:
         products = paginator.page(paginator.num_pages)
 
-    return render(request,'store/store.html',{'products':products,'categories':categories})
+    return render(request, 'store/store.html', {
+        'products': products,
+        'categories': categories,
+        'category_id': c,
+    })
 
-def detail(request,slug):
-    product = get_object_or_404(Product,slug=slug)
-    return render(request,'store/detail.html',{
-        'product':product,
+
+def detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'store/detail.html', {
+        'product': product,
+    })
+
+
+def cart_add(request, slug):
+    product = get_object_or_404(Book, slug=slug)
+    cart_items = request.session('cart_items') or []
+
+    duplicated = False
+
+    for c in cart_items:
+        if c.get('slug') == product.slug:
+            c['qty'] = int(c.get('qty') or '1') + 1
+            duplicated = True
+
+    if not duplicated:
+        cart_items.append({
+            'id' : product.id,
+            'slug' : product.slug,
+            'name' : product.name,
+            'qty': 1,
+        })
+
+    return HttpResponseRedirect('myapp:cart_list', kwargs={})
+
+
+def cart_list(request):
+    cart_items = request.session('cart_items') or []
+
+    total_qty = 0
+
+    for c in cart_items:
+        total_qty = total_qty + c.get('qty')
+    
+    request.session['cart_qty'] = total_qty
+    return render(request, 'store/cart.html', {
+        'cart_items' : cart_items,
     })
