f9d925af6fa2808e82ae0f9a51747384b2466b1d
Peak793 <69148111+Peak793@users.noreply.github.com>
2021-11-25 22:49:10 +0700

    -cart build1 finished

--- store/myapp/urls.py
+++ store/myapp/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('store/<slug:slug>/',views.detail,name='detail'),
     re_path(r'cart/add/(?P<slug>[\w-]+)/$', views.cart_add, name ='cart_add'),
     re_path(r'cart/delete/(?P<slug>[\w-]+)/$', views.cart_delete, name ='cart_delete'),
+    re_path(r'cart/inc/(?P<slug>[\w-]+)/$', views.inc_qty, name ='inc_qty'),
+    re_path(r'cart/dec/(?P<slug>[\w-]+)/$', views.dec_qty, name ='dec_qty'),
     re_path(r'cart/list/$', views.cart_list, name ='cart_list'),
 ]
--- store/myapp/views.py
+++ store/myapp/views.py
@@ -48,12 +48,13 @@ def detail(request, slug):
 def cart_add(request, slug):
     product = get_object_or_404(Product, slug=slug)
     cart_items = request.session.get('cart_items') or []
-
+    total_price = int(request.session.get('total_price')) or 0
     duplicated = False
 
     for c in cart_items:
         if c.get('slug') == product.slug:
             c['qty'] = int(c.get('qty') or '1') + 1
+            c['total'] = int(c.get('qty') or '1') * int(c.get('price'))
             duplicated = True
 
     if not duplicated:
@@ -64,22 +65,29 @@ def cart_add(request, slug):
             'name' : product.name,
             'price' : product.price,
             'qty': 1,
+            'total': int(product.price),
         })
     request.session['cart_items'] = cart_items 
+    request.session['total_price'] = total_price
     return HttpResponseRedirect(reverse('myapp:cart_list', kwargs={}))
 
 
 def cart_list(request):
     cart_items = request.session.get('cart_items') or []
-
+    total_price = int(request.session.get('total_price')) or 0
     total_qty = 0
+    total_item = 0
 
     for c in cart_items:
         total_qty = total_qty + c.get('qty')
+        total_price += int(c.get('total'))
+        total_item += 1
     
     request.session['cart_qty'] = total_qty
     return render(request, 'store/cart.html', {
         'cart_items' : cart_items,
+        'total_price' : total_price,
+        'total_item' : total_item,
     })
 
 def cart_delete(request,slug):
@@ -88,5 +96,24 @@ def cart_delete(request,slug):
         if cart_items[i]['slug'] == slug:
             del cart_items[i]
             break
+    request.session['cart_items'] = cart_items
+    return HttpResponseRedirect(reverse('myapp:cart_list',kwargs={}))
+
+def inc_qty(request,slug):
+    cart_items = request.session.get("cart_items") or []
+    for i in range(len(cart_items)):
+        if cart_items[i]['slug'] == slug:
+            cart_items[i]['qty'] = int(cart_items[i]['qty']) + 1
+            cart_items[i]['total'] = int(cart_items[i]['price']) * int(cart_items[i]['qty'])
+    request.session['cart_items'] = cart_items
+    return HttpResponseRedirect(reverse('myapp:cart_list',kwargs={}))
+
+def dec_qty(request,slug):
+    cart_items = request.session.get("cart_items") or []
+    for i in range(len(cart_items)):
+        if cart_items[i]['slug'] == slug:
+            if int(cart_items[i]['qty']) > 1:
+                cart_items[i]['qty'] = int(cart_items[i]['qty']) - 1
+                cart_items[i]['total'] = int(cart_items[i]['price']) * int(cart_items[i]['qty'])
     request.session['cart_items'] = cart_items
     return HttpResponseRedirect(reverse('myapp:cart_list',kwargs={}))
